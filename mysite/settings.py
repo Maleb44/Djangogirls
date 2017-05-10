8518d7acb70d3aac18e09d7cd04c710c5abbedbe
Alex <maleb44@yahoo.com>
2017-05-10 09:28:17 +0200

    My Django Girls app, 2017.0510  commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,6 +99,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+## modositas 2017/05/09
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#
 STATIC_URL = '/static/'
 STATIC_URL = os.path.join(BASE_DIR,'/static/')
